71f4969d21c6f33b7c18ccfda2132caeace5f82f
Tuan Le <tuan.le94.tl@gmail.com>
2018-02-06 13:26:24 +0000

    This is an extension of cinemas with a json converter.

--- Cinemas/src/Main.java
+++ Cinemas/src/Main.java
@@ -3,22 +3,23 @@
 public class Main {
 
 	public static void main(String[] args) {
-		service s = new service();
-		movies j = new movies("Jumanji: Welcome to the jungle","Fantasy", "12a");
-		movies h = new movies("Harry Potter and the order of the pheonix", "Adventure", "PG");	
-		movies a = new movies("Avengers: Age of Ultron", "Action", "15");
-		movies b = new movies("Spectre", "Action", "18");
+		Service service = new Service();
+		Movies j = new Movies("Jumanji: Welcome to the jungle","Fantasy", "12a");
+		Movies h = new Movies("Harry Potter and the order of the pheonix", "Adventure", "PG");	
+		Movies a = new Movies("Avengers: Age of Ultron", "Action", "15");
+		Movies b = new Movies("Spectre", "Action", "18");	
 		
-		 
-				
-		s.addMovies(1,j);	
-		s.addMovies(2, h);
-		s.addMovies(3, a);
-		s.addMovies(4, b);
-		
-		System.out.println(s.getMovies(1));
-		System.out.println(s.getMovies(3));
+		service.addMovies(1,j);	
+		service.addMovies(2, h);
+		service.addMovies(3, a);
+		service.addMovies(4, b);
+
+		System.out.println(service.getMovies(1));
+		System.out.println(service.getMovies(3));
+		System.out.println(service.converter());
 	}
+	
+	
 
 
 }
