e7e7adafc6dcd504efeff88f444f067873b3ec51
Tuan Le <tuan.le94.tl@gmail.com>
2018-02-06 15:51:26 +0000

    test#1

--- Cinemas/src/Service.java
+++ Cinemas/src/Service.java
@@ -5,16 +5,32 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Service {
 	HashMap<Integer, Movies> moviesMap = new HashMap<Integer, Movies>();
-	
-	public void addMovies(Integer x, Movies m) {
-		moviesMap.put(x,m);
+
+	public Movies createMovie(String name, String genre, String ageRating) {
+		Movies movie = new Movies(name, genre, ageRating);
+		return movie;
 	}
-	
-	public  Movies getMovies(Integer x) {
+
+	public void generateMap() {
+
+		addMovies(1, createMovie("Jumanji: Welcome to the jungle", "Fantasy", "12a"));
+		addMovies(2, createMovie("Harry Potter and the order of the pheonix", "Adventure", "PG"));
+		addMovies(3, createMovie("Avengers: Age of Ultron", "Action", "15"));
+		addMovies(4, createMovie("Spectre", "Action", "18"));
+		addMovies(5, createMovie("Paranormal Activity", "Horror", "18"));
+
+	}
+
+	public void addMovies(Integer x, Movies m) {
+		moviesMap.put(x, m);
+	}
+
+	public Movies getMovies(Integer x) {
 		return moviesMap.get(x);
 	}
+
 	public String converter(int Y) {
-		
+
 		String json = null;
 		try {
 			json = new ObjectMapper().writeValueAsString(moviesMap.get(Y));
@@ -24,7 +40,7 @@ public class Service {
 		}
 		return json;
 	}
-	
-}
-	
 
+
+
+}
--- Cinemas/src/TestingForCinemas.java
+++ Cinemas/src/TestingForCinemas.java
@@ -1,12 +1,25 @@
 import static org.junit.Assert.*;
 
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestingForCinemas {
 
+	Service service = new Service();
+	@Before
+	public void before() {
+		
+		
+	}
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		service.generateMap();
+		int expectedCount= 2;
+		int actualCount = service.countGenre("Action");
+		assertEquals(actualCount,expectedCount);
+		
+		
 	}
 
 }
