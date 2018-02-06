17ad8983f76f9d2d711836bf023437e1fc9cece3
Tuan Le <tuan.le94.tl@gmail.com>
2018-02-06 15:54:07 +0000

    test#2.2, just adding an updated Main

--- Cinemas/src/Main.java
+++ Cinemas/src/Main.java
@@ -5,17 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		Service service = new Service();
 		
-		Movies movie1 = new Movies("Jumanji: Welcome to the jungle","Fantasy", "12a");
-		Movies movie2 = new Movies("Harry Potter and the order of the pheonix", "Adventure", "PG");	
-		Movies movie3 = new Movies("Avengers: Age of Ultron", "Action", "15");
-		Movies movie4 = new Movies("Spectre", "Action", "18");
-		Movies movie5 = new Movies("Paranormal Activity", "Horror", "18");
-		
-		service.addMovies(1,movie1);	
-		service.addMovies(2,movie2);
-		service.addMovies(3,movie3);
-		service.addMovies(4,movie4);
-		service.addMovies(5, movie5);
 
 		System.out.println(service.getMovies(1));
 		System.out.println(service.getMovies(3));
